3ed7235dfcded6b54b88d5e58a0631aadc27fa6c
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-01-22 13:26:41 +0900

    grant

--- db/scripts/grant_privileges.rb
+++ db/scripts/grant_privileges.rb
@@ -9,19 +9,19 @@ configurations = YAML.load_file(path)
 config = configurations[env]
 
 statements = %Q{
-  ALTER USER #{config['username']} WITH PASSWORD '#{config['password']}';
+  ALTER USER #{config["username"]} WITH PASSWORD '#{config["password"]}';
 
   GRANT SELECT, INSERT, UPDATE, DELETE
   ON ALL TABLES IN SCHEMA public
-  TO #{config['username']}
-}.gsub(/\s+/, ' ').strip
+  TO #{config["username"]}
+}.gsub(/\s+/, " ").strip
 
 config = configurations["migrator"]
 
 command = %Q{
-  PGPASSWORD=#{config['password']} psql -U #{config['username']} --quiet
-  --host=#{config['host']} --port=#{config['port']}
-  #{config['database']} -c "#{statements}"
-}.gsub(/\s+/, ' ').strip
+  PGPASSWORD=#{config["password"]} psql -U #{config["username"]} --quiet
+  --host=#{config["host"]} --port=#{config["port"]}
+  #{config["database"]} -c "#{statements}"
+}.gsub(/\s+/, " ").strip
 
 system(command)
